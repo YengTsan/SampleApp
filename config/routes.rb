48ed88220e59765b9c036f44badabd7aeb966a08
Yeng Tsan <yengttt@gmail.com>
2016-08-08 02:00:32 +0800

    user url helper in rails convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root 'static_page#home'
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/about'
-  get 'static_page/contact'
+  get '/home', to: 'static_page#home', as: 'helf'
+  get '/help', to: 'static_page#help'
+  get '/about', to: 'static_page#about'
+  get '/contact', to: 'static_page#contact'
 
 end
